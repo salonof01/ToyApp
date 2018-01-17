52d7076e5dc122cbc11dc512735c2a190dc54c26
onofresalvador <onofresalvador01@gmail.com>
2018-01-16 20:42:36 -0500

    Second face 01

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,9 +17,13 @@ package com.example.android.datafrominternet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+  private EditText mSearchBoxEditText;
+   private TextView mUrlDisplayTextView;
+  private TextView mSearchResultsTextView;
     // TODO (26) Create an EditText variable called mSearchBoxEditText
 
     // TODO (27) Create a TextView variable called mUrlDisplayTextView
@@ -29,10 +33,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+// TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+        mSearchBoxEditText=(EditText)findViewById(R.id.et_search_box);
 
         // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
+
+        mUrlDisplayTextView=(TextView)findViewById(R.id.tv_url_display);
         // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        mSearchResultsTextView=(TextView)findViewById(R.id.tv_github_search_result_json);
     }
 }
