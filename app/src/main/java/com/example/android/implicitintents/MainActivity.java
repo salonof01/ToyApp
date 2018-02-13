22f9242ff1688cdcba6c89b03333223c86ed428c
onofresalvador <onofresalvador01@gmail.com>
2018-02-12 19:39:02 -0500

    Learn to open map

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -49,11 +49,18 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
+        String lookUpAddress =" 9608 SW 3 Lane, Miami Florida";
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .path("0,0")
+                .query(lookUpAddress);
+        Uri addressPlace = builder.build();
+
 
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
 
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();showMap(addressPlace);
     }
 
     /**
@@ -114,6 +121,17 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a method called showMap with a Uri as the single parameter
     // Do steps 2 - 4 within the showMap method
+         public void showMap(Uri placeLocation){
+
+          Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(placeLocation);
+            if (intent.resolveActivity(getPackageManager()) !=null){
+                startActivity(intent);
+            }
+
+
+         }
+
         // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
 
         // TODO (3) Set the data of the Intent to the Uri passed into this method
